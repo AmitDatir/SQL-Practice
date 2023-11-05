098ff12d9fa8aa792550a845fc0107fa0ec9e85e
Amit Datir <106661557+AmitDatir@users.noreply.github.com>
2023-11-05 15:07:30 +0530

    Update 1484. Group Sold Products By The Date.sql

--- LeetCode/1484. Group Sold Products By The Date.sql
+++ LeetCode/1484. Group Sold Products By The Date.sql
@@ -1,3 +1,6 @@
 -- Que.: 1484. Group Sold Products By The Date: https://leetcode.com/problems/group-sold-products-by-the-date/
 --Solution:
-SELECT sell_date, COUNT(DISTINCT(product)) AS num_sold, GROUP_CONCAT(DISTINCT product ORDER BY product) AS products FROM Activities GROUP BY sell_date;
+SELECT sell_date, COUNT(DISTINCT(product)) AS num_sold, 
+GROUP_CONCAT(DISTINCT product ORDER BY product) AS products 
+FROM Activities 
+GROUP BY sell_date;
